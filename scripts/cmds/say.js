f77ea97af428c567abc665d119eaff9ed419006a
tashfir90-cmd <tashfir90@gmail.com>
2026-03-16 10:41:16 +0000

    Update say.js

--- scripts/cmds/say.js
+++ scripts/cmds/say.js
@@ -1,4 +1,4 @@
-module.exports.config = {
+bn.exports.config = {
 	name: "say",
 	version: "1.6.9",
 	role: 0,
@@ -20,7 +20,7 @@ module.exports.onStart = async function({ api, event, args }) {
 		const { createReadStream, unlinkSync } = require('fs-extra');
 		const { resolve } = require('path');
 		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
-		var languageToSay = (["bn",].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.GoatBot.config.language;
+		var languageToSay = (["ar",].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.GoatBot.config.language;
 		var msg = (languageToSay != global.GoatBot.config.language) ? content.slice(3, content.length) : content;
 		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
 		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=bn&client=tw-ob`, path);
