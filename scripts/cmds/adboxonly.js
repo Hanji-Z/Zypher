3954ceefff586bb526539b1f48e2b75a4f78f330
tashfir <tashfir90@gmail.com>
2026-03-30 22:53:11 +0100

    Update adboxonly.js

--- scripts/cmds/adboxonly.js
+++ scripts/cmds/adboxonly.js
@@ -8,7 +8,7 @@ module.exports = {
 		role: 2,
 		description: {
 			vi: "bật/tắt chế độ chỉ quản trị của viên nhóm mới có thể sử dụng bot",
-			en: "turn on/off only admin box can use bot"
+			en: "turn on/off only admin 7box can use bot"
 		},
 		category: "𝗕𝗢𝗫 𝗖𝗛𝗔𝗧",
 		guide: {
