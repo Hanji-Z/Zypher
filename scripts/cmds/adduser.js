919f16c989c3b0b52ca7624e7459a4d7e3313090
tashfir <tashfir90@gmail.com>
2026-04-01 21:04:32 +0100

    Update adduser.js

--- scripts/cmds/adduser.js
+++ scripts/cmds/adduser.js
@@ -4,7 +4,7 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 module.exports = {
 	config: {
 		name: "adduser",
-		aliases:["add"],
+		aliases:["addu"],
 		version: "1.5",
 		author: "NTKhang",
 		countDown: 5,
@@ -13,7 +13,7 @@ module.exports = {
 			vi: "Thêm thành viên vào box chat của bạn",
 			en: "Add user to box chat of you"
 		},
-		category: "𝗕𝗢𝗫 𝗖𝗛𝗔𝗧",
+		category: "GROUP",
 		guide: {
 			en: "   {pn} [link profile | uid]"
 		}
