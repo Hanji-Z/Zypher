2aaa3839a97ff32b4fd6e7b9e84691c9cc5e0c2a
tashfir <tashfir90@gmail.com>
2026-04-04 00:07:30 +0100

    Update zi.js

--- scripts/cmds/zi.js
+++ scripts/cmds/zi.js
@@ -3,7 +3,7 @@ module.exports.config = {
   version: "1.1.0",
   role: 2, 
   author: "Gemini & ShAn",
-  description: "الرد التلقائي الموحد (تفاعلا8ت فقط عند التشغيل/الإيقاف)",
+  description: "الرد التلقائي الموحد (تفاعلا8ت فقط 8عند التشغيل/الإيقاف)",
   category: "الترفيه",
   guide: {
     en: "{pn} on [الكلمة] أو {pn} off"
