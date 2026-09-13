2033aa6e3133fcb320aff6d2f1a4000e489b5cd2
Hanji-Z <kathleen78888@outlook.com>
2026-09-13 22:41:23 +0000

    Enforce cookie-only login

--- bot/login/login.js
+++ bot/login/login.js
@@ -399,7 +399,7 @@ let spin;
 async function getAppStateToLogin(loginWithEmail) {
 	let appState = [];
 	if (loginWithEmail)
-		return await getAppStateFromEmail(undefined, facebookAccount);
+		throw new Error("Credential login is disabled; provide a valid cookie file");
 	if (!existsSync(dirAccount))
 		return log.error("LOGIN FACEBOOK", getText('login', 'notFoundDirAccount', colors.green(dirAccount)));
 	const accountText = readFileSync(dirAccount, "utf8");
@@ -500,106 +500,13 @@ async function getAppStateToLogin(loginWithEmail) {
 	}
 	catch (err) {
 		spin && spin._stop();
-		let {
-			email,
-			password
-		} = facebookAccount;
 		if (err.name === "TOKEN_ERROR")
 			log.err("LOGIN FACEBOOK", getText('login', 'tokenError', colors.green("EAAAA..."), colors.green(dirAccount)));
 		else if (err.name === "COOKIE_INVALID")
 			log.err("LOGIN FACEBOOK", getText('login', 'cookieError'));
 
-		if (!email || !password) {
-			log.warn("LOGIN FACEBOOK", getText('login', 'cannotFindAccount'));
-			const rl = readline.createInterface({
-				input: process.stdin,
-				output: process.stdout
-			});
-			const options = [
-				getText('login', 'chooseAccount'),
-				getText('login', 'chooseToken'),
-				getText('login', 'chooseCookieString'),
-				getText('login', 'chooseCookieArray')
-			];
-			let currentOption = 0;
-			await new Promise((resolve) => {
-				const character = '>';
-				function showOptions() {
-					rl.output.write(`\r${options.map((option, index) => index === currentOption ? colors.blueBright(`${character} (${index + 1}) ${option}`) : `  (${index + 1}) ${option}`).join('\n')}\u001B`);
-					rl.write('\u001B[?25l'); // hides cursor
-				}
-				rl.input.on('keypress', (_, key) => {
-					if (key.name === 'up') {
-						currentOption = (currentOption - 1 + options.length) % options.length;
-					}
-					else if (key.name === 'down') {
-						currentOption = (currentOption + 1) % options.length;
-					}
-					else if (!isNaN(key.name)) {
-						const number = parseInt(key.name);
-						if (number >= 0 && number <= options.length)
-							currentOption = number - 1;
-						process.stdout.write('\033[1D'); // delete the character
-					}
-					else if (key.name === 'enter' || key.name === 'return') {
-						rl.input.removeAllListeners('keypress');
-						rl.close();
-						clearLines(options.length + 1);
-						showOptions();
-						resolve();
-					}
-					else {
-						process.stdout.write('\033[1D'); // delete the character
-					}
-
-					clearLines(options.length);
-					showOptions();
-				});
-				showOptions();
-			});
-
-			rl.write('\u001B[?25h\n'); // show cursor 
-			clearLines(options.length + 1);
-			log.info("LOGIN FACEBOOK", getText('login', 'loginWith', options[currentOption]));
-
-			if (currentOption == 0) {
-				email = await input(`${getText('login', 'inputEmail')} `);
-				password = await input(`${getText('login', 'inputPassword')} `, true);
-				const twoFactorAuth = await input(`${getText('login', 'input2FA')} `);
-				facebookAccount.email = email || '';
-				facebookAccount.password = password || '';
-				facebookAccount['2FASecret'] = twoFactorAuth || '';
-				writeFileSync(global.client.dirConfig, JSON.stringify(global.GoatBot.config, null, 2));
-			}
-			else if (currentOption == 1) {
-				const token = await input(getText('login', 'inputToken') + " ");
-				writeFileSync(global.client.dirAccount, token);
-			}
-			else if (currentOption == 2) {
-				const cookie = await input(getText('login', 'inputCookieString') + " ");
-				writeFileSync(global.client.dirAccount, cookie);
-			}
-			else {
-				const cookie = await input(getText('login', 'inputCookieArray') + " ");
-				writeFileSync(global.client.dirAccount, JSON.stringify(JSON.parse(cookie), null, 2));
-			}
-			return await getAppStateToLogin();
-		}
-
-		log.info("LOGIN FACEBOOK", getText('login', 'loginPassword'));
-		log.info("ACCOUNT INFO", `Email: ${facebookAccount.email}, I_User: ${facebookAccount.i_user || "(empty)"}`);
-		spin = createOraDots(getText('login', 'loginPassword'));
-		spin._start();
-
-		try {
-			appState = await getAppStateFromEmail(spin, facebookAccount);
-			spin._stop();
-		}
-		catch (err) {
-			spin._stop();
-			log.err("LOGIN FACEBOOK", getText('login', 'loginError'), err.message, err);
-			process.exit();
-		}
+		log.err("LOGIN FACEBOOK", "Cookie login failed; credential login is disabled", err.message);
+		process.exit(1);
 	}
 	return appState;
 }
